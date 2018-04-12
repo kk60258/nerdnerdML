d08e6c549eeb99b424d4073f24eb73deff43e572
shun <u8810242@gmail.com>
2018-04-12 13:03:16 +0800

    Save&Restore the model

--- mnist_softmax_loader.py
+++ mnist_softmax_loader.py
@@ -1,24 +1,11 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
-# Copyright 2015 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-
-"""A very simple MNIST classifier.
-See extensive documentation at
-https://www.tensorflow.org/get_started/mnist/beginners
 """
+Created on Thu Apr 12 11:06:42 2018
+
+@author: shun
+"""
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -31,18 +18,19 @@ from tensorflow.examples.tutorials.mnist import input_data
 # import the inspect_checkpoint library
 from tensorflow.python.tools import inspect_checkpoint as chkp
 
-
-import tensorflow as tf
-
 #ensure that all elements are printed
 import numpy
 numpy.set_printoptions(threshold=numpy.nan)
+
+import tensorflow as tf
 
 FLAGS = None
 
 
 def main(_):
+ 
   tf.reset_default_graph()
+    
   # Import data
   mnist = input_data.read_data_sets(FLAGS.data_dir, one_hot=True)
 
@@ -59,36 +47,11 @@ def main(_):
   # Define loss and optimizer
   y_ = tf.placeholder(tf.float32, [None, 10])
 
-  # The raw formulation of cross-entropy,
-  #
-  #   tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(tf.nn.softmax(y)),
-  #                                 reduction_indices=[1]))
-  #
-  # can be numerically unstable.
-  #
-  # So here we use tf.nn.softmax_cross_entropy_with_logits on the raw
-  # outputs of 'y', and then average across the batch.
-  cross_entropy = tf.reduce_mean(
-      tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
-  # 0.5 -> 0.05
-  train_step = tf.train.GradientDescentOptimizer(0.05).minimize(cross_entropy)
 
   sess = tf.InteractiveSession()
-  tf.global_variables_initializer().run()
   
-  #write log file for tensorBoard
-  writer = tf.summary.FileWriter("/tmp/shun_graph", graph=tf.get_default_graph())
-  
-  # Train
-  for _ in range(1000):
-    batch_xs, batch_ys = mnist.train.next_batch(100)
-    sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
-
-  writer.close()
-
-  # Save the variables to disk.
-  save_path = tf.train.Saver().save(sess, "/tmp/shun_model.ckpt")
-  print("Model saved in path: %s" % save_path)
+  # load saved model
+  tf.train.Saver().restore(sess, "/tmp/shun_model.ckpt")
   
   # print all tensors in checkpoint file
   chkp.print_tensors_in_checkpoint_file("/tmp/shun_model.ckpt", tensor_name='', all_tensors=True)
