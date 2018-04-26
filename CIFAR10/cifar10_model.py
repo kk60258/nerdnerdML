07423b301ae241cb2a370d789f9bfd782f06c5be
kk60258 <kk60258@gmail.com>
2018-04-27 01:08:39 +0800

    change model

--- CIFAR10/cifar10_model.py
+++ CIFAR10/cifar10_model.py
@@ -18,25 +18,25 @@ def inference(images):
     # layer1
     with tf.variable_scope('layer1'):
         model = tf.layers.conv2d(inputs=images, filters=32, kernel_size=[5, 5], strides=(1, 1), padding='same', activation=tf.nn.relu, kernel_regularizer=tf.contrib.layers.l2_regularizer(0.1), name='conv')
-        model = tf.layers.max_pooling2d(model, pool_size=[3, 3], strides=[2, 2], padding='same', name='pool')
+        model = tf.layers.max_pooling2d(model, pool_size=[2, 2], strides=[2, 2], padding='same', name='pool')
         # model = tf.layers.batch_normalization(inputs=model, name='batch')
 
     # layer2
     with tf.variable_scope('layer2'):
-        model = tf.layers.conv2d(inputs=model, filters=32, kernel_size=[5, 5], strides=(1, 1), padding='same', activation=tf.nn.relu, kernel_regularizer=tf.contrib.layers.l2_regularizer(0.1), name='conv')
-        model = tf.layers.average_pooling2d(inputs=model, pool_size=[3, 3], strides=[2, 2], padding='same', name='pool')
+        model = tf.layers.conv2d(inputs=model, filters=64, kernel_size=[5, 5], strides=(1, 1), padding='same', activation=tf.nn.relu, kernel_regularizer=tf.contrib.layers.l2_regularizer(0.1), name='conv')
+        model = tf.layers.average_pooling2d(inputs=model, pool_size=[2, 2], strides=[2, 2], padding='same', name='pool')
         # model = tf.layers.batch_normalization(inputs=model, name='batch')
 
     # layer3
     with tf.variable_scope('layer3'):
-        model = tf.layers.conv2d(inputs=model, filters=64, kernel_size=[5, 5], strides=(1, 1), padding='same', activation=tf.nn.relu, kernel_regularizer=tf.contrib.layers.l2_regularizer(0.1), name='conv')
-        model = tf.layers.average_pooling2d(inputs=model, pool_size=[3, 3], strides=[2, 2], padding='same', name='pool')
+        model = tf.layers.conv2d(inputs=model, filters=128, kernel_size=[3, 3], strides=(1, 1), padding='same', activation=tf.nn.relu, kernel_regularizer=tf.contrib.layers.l2_regularizer(0.1), name='conv')
+        model = tf.layers.average_pooling2d(inputs=model, pool_size=[2, 2], strides=[2, 2], padding='same', name='pool')
         # model = tf.layers.batch_normalization(inputs=model, name='batch')
 
     # layer4
     with tf.variable_scope('layer4'):
         model = tf.layers.flatten(inputs=model, name='flatten')
-        model = tf.layers.dense(inputs=model, units=64, activation=tf.nn.relu, kernel_regularizer=tf.contrib.layers.l2_regularizer(0.1), name='dense1')
+        model = tf.layers.dense(inputs=model, units=128, activation=tf.nn.relu, kernel_regularizer=tf.contrib.layers.l2_regularizer(0.1), name='dense1')
         model = tf.layers.dense(inputs=model, units=10, activation=tf.nn.relu, kernel_regularizer=tf.contrib.layers.l2_regularizer(0.1), name='dense2')
     # We don't apply softmax here because
     # and performs the softmax internally for efficiency.
